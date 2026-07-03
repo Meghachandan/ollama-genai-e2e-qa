07e99e97d2810a546a07b907c4822850bff36b47
besta megha chandan kumar <meghachandankumar2@gmail.com>
2026-07-03 10:29:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
+
 
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-# Load environment variables
-load_dotenv()
-
-# LangSmith Tracking (Optional)
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY", "")
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "Simple Q&A Chatbot With Ollama"
 
 # Prompt Template
 prompt = ChatPromptTemplate.from_messages(
